02e0eabc198fc94b597e117ec91d6f4d0e3a318e
Marie <knapplau@oregonstate.edu>
2020-02-28 22:04:20 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -12,6 +12,11 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+    def test3(self):
+        radius = 3
+        area = task.circleArea(radius)
+        self.assertEqual(18.84, area)
+
 
 if __name__ == ' __main__':
     unittest.main()
