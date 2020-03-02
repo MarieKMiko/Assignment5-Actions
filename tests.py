507722c6aa650aefedd2ed188fefca44775af42c
Marie <knapplau@oregonstate.edu>
2020-03-01 18:27:47 -0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 import task
-
+from datetime import date
 
 class TestCase(unittest.TestCase):
 
@@ -21,6 +21,12 @@ class TestCase(unittest.TestCase):
         list = [24, 38, 19]
         last = task.listLast(list)
         self.assertEqual(19, last)
+
+    def test5(self):
+        date1 = 3/13/20
+        date2 = 4/13/20
+        difference = task.dateDiff(date1, date2)
+        self.assertEqual(31, difference)
 
 
 if __name__ == ' __main__':
